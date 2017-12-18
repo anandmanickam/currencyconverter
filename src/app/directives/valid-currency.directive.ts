a74a8229418091c6eaa2d96933009341ea03ece2
Anand Manickam <anand.manickam@hotmail.com>
2017-12-19 00:25:56 +0530

    minor bug fix - numpad period detection for valid currency directive

--- src/app/directives/valid-currency.directive.ts
+++ src/app/directives/valid-currency.directive.ts
@@ -11,14 +11,14 @@ export class ValidCurrencyDirective {
     @HostListener('keydown', ['$event']) onKeyDown(event) {
         let e = <KeyboardEvent> event;
         if (this.validCurrency) {
-            if (e.which == 64 || e.which == 16) {  
+            if (e.which === 64 || e.which === 16) {  
                 return true;  
             } else if (e.which >= 48 && e.which <= 57) {  
                 return true;  
             } else if (e.which >= 96 && e.which <= 105) {  
                 return true;  
-            } else if ([8, 9, 13, 27, 37, 38, 39, 40, 190].indexOf(e.which) > -1) {  
-                if(e.which === 190 && event.target.value.indexOf('.') > -1 ){
+            } else if ([8, 9, 13, 27, 37, 38, 39, 40, 110, 190].indexOf(e.which) > -1) {  
+                if((e.which === 190 || e.which === 110) && event.target.value.indexOf('.') > -1 ){
                     return false;
                 }
             } else {  
